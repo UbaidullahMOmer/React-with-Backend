3d626eb399dcd7fc9d6b82f6570dcf3682d05bbc
Heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-02-03 07:46:18 -0800

    nerro

--- frontend/src/pages/SinglePortfolio.js
+++ frontend/src/pages/SinglePortfolio.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 const SinglePortfolio = () => {
     const { id } = useParams();
-    const { loading, error, data } = useFetch("http://localhost:3000/portfolio/" + id)
+    const { loading, error, data } = useFetch('http://localhost:3000/portfolio/' + id)
+    console.log("dsd",data)
     if (loading) return <div>Loading...</div>
     if (error) return <div>Error...</div>
 
-    console.log(data)
-    return (
-        <>
-            <h1>Single Portfolio</h1>
-            <h2>{data.data.attributes.title}</h2>
-            <p>{data.data.attributes.descritpion}</p>
-        </>
-    )
     return (
         <div>
             SinglePortfolio
