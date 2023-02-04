6e32bfd10ffac6ef50ad99b08f201ca73a323d50
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-04 02:22:53 -0800

    Add css or error solve

--- frontend/src/pages/SinglePortfolio.js
+++ frontend/src/pages/SinglePortfolio.js
@@ -4,14 +4,22 @@ import useFetch from '../hooks/useFetch'
 
 const SinglePortfolio = () => {
     const { id } = useParams();
-    const {loading, error, data} = useFetch('http://localhost:1337/api/portfolio' + id)
+    const {loading, error, data} = useFetch('http://localhost:1337/api/portfolios/' + id + '?populate=image')
 
     if (loading) return <div>Loading...</div>
     if (error) return <div>Errorxdf...</div>
+
     
     console.log("dsd",data)
+    console.log(`http://localhost:1337${data.data.attributes.image.data.attributes.url}`)
     return (
-        <div>SinglePortfolio</div>
+        <>
+        <h1>SinglePortfolio</h1>
+        <h2>{data.data.attributes.title}</h2>
+        <img src={`http://localhost:1337${data.data.attributes.image.data.attributes.url}`}/>
+        <span>{data.data.attributes.date}</span>
+        <p>{data.data.attributes.description}</p>
+        </>
     )
 }
 
