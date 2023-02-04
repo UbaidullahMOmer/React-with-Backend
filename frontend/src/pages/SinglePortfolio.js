81477f5635e3227aff890c2b0e6010bad633932c
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-04 01:37:23 -0800

    solve the error
    1337 url change

--- frontend/src/pages/SinglePortfolio.js
+++ frontend/src/pages/SinglePortfolio.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
-import { useParams } from 'react-router-dom'
 
 const SinglePortfolio = () => {
     const { id } = useParams();
-    const { loading, error, data } = useFetch('http://localhost:3000/portfolio/' + id)
+    const {loading, error, data} = useFetch('http://localhost:1337/api/portfolio' + id)
+
     if (loading) return <div>Loading...</div>
     if (error) return <div>Errorxdf...</div>
     
     console.log("dsd",data)
     return (
-        <div>
-            SinglePortfolio
-        </div>
+        <div>SinglePortfolio</div>
     )
 }
 
