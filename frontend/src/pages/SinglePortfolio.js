7df5ad161264c60e4c8746d986ac3e3db755b659
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-06 06:16:45 -0800

    add gql in apollo

--- frontend/src/pages/SinglePortfolio.js
+++ frontend/src/pages/SinglePortfolio.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
+import {useQuery, gql } from '@apollo/client'
 
+const PORTFOLIOS = gql`
+query GetPortfolios{
+ portfolios{
+  data{
+    id
+    attributes{
+     title,
+     description
+    }
+  }
+}
+}
+`
 const SinglePortfolio = () => {
     const { id } = useParams();
     const {loading, error, data} = useFetch('http://localhost:1337/api/portfolios/' + id + '?populate=image')
+
 
     if (loading) return <div>Loading...</div>
     if (error) return <div>Errorxdf...</div>
