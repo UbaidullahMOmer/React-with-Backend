a63d54be7ad0c1f17c24ef7ced3e4d448a579bb5
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-06 01:18:20 -0800

    formate file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,18 @@ import Category from "./pages/Category"
 import { ApolloProvider } from '@apollo/client';
 import client from './utils/apoloClient';
 function App() {
-  
+
   return (
     <div className="App">
       <BrowserRouter>
-      <ApolloProvider client={client}>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/tag/:id' element={<Tag/>}  /> 
-          <Route path='/category/:id' element={<Category/>}  />
-          <Route path='/portfolio/:id' element={<SinglePortfolio/>} /> 
-        </Routes>
-      </ApolloProvider>
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/tag/:id' element={<Tag />} />
+            <Route path='/category/:id' element={<Category />} />
+            <Route path='/portfolio/:id' element={<SinglePortfolio />} />
+          </Routes>
+        </ApolloProvider>
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import useFetch from '../hooks/useFetch'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const HomePage = () => {
-    const {loading, error, data} = useFetch('http://localhost:1337/api/portfolios')
+  const { loading, error, data } = useFetch('http://localhost:1337/api/portfolios')
 
-    console.log("dftgdg",data)
-    if (loading) return <div>Loading...</div>
-    if (error) return <div>Errhhhor...</div>
- 
+  console.log("dftgdg", data)
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Errhhhor...</div>
+
   return (
     <div>
-        <>
+      <>
         <h1>Home Page</h1>
         {data.data.map(item => (
-            <div key={item.id}>
+          <div key={item.id}>
             <h2>{item.attributes.title}</h2>
-            <p>{item.attributes.description.substring(0,150)}</p>
+            <p>{item.attributes.description.substring(0, 150)}</p>
             <Link to={`/portfolio/${item.id}`}>View</Link>
-            </div>
+          </div>
         ))}
-        </>
+      </>
     </div>
   )
 }
