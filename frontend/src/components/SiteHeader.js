788d4fe48e0adf3edc023e8b96ee9700f380b807
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-07 12:47:24 +0500

    nav bar created

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import {Navbar,Nav} from "react-bootstrap"
+import { gql, useQuery } from '@apollo/client'
+
+const CATEGORY = gql`
+    query GetCategories{
+        categories{
+            deta{
+                id,
+                attributes{
+                    name,
+                    slug
+                }
+            }
+        }
+    }
+`
 
 function SiteHeader() {
+    const {loading, error, data} = useQuery(CATEGORIES)
+
+    if(loading) return <div><h1>Loading...</h1></div>
+    if(loading) return <div><h1>Error...</h1></div>
   return (
     <>
     <Navbar className="py-3"  sticky='top' collapseOnSelect expand="lg">
@@ -9,10 +28,11 @@ function SiteHeader() {
         <Navbar.Toggle aria-controls='responsive-navbar-nav'></Navbar.Toggle>
         <Navbar.Collapse className='justify-content-end' id="responsive-navbar-nav">
             <Nav>
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/'>Web</Nav.Link>
-                <Nav.Link href='/'>Mobile</Nav.Link>
-                <Nav.Link href='/'>Cloud</Nav.Link>
+                {
+                    data.categories.data.map(item => (
+                        
+                    ))
+                }
             </Nav>
         </Navbar.Collapse>
     </Navbar>
