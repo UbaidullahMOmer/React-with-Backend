d0c5f4b73522c16f2873bdb8ea4e1e138321f807
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-10 14:19:20 +0500

    category page ready

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {gql, useQuery} from '@apollo/client'
-import {Row, Col, Card} from 'react-bootstrap'
-import {useParams} from 'react-router-dom'
+import {Row, Col, Card, Container} from 'react-bootstrap'
+import {Link, useParams} from 'react-router-dom'
 
 const CATEGORIES = gql`
 query GetCategory ($id:ID!) {
@@ -21,7 +21,7 @@ query GetCategory ($id:ID!) {
               image{
                 data{
                   attributes{
-                    formats
+                    url
                   }
                 }
               }
@@ -48,13 +48,23 @@ console.log("sdfsdfsdf",id)
 
 return(
   <div>
+    <Container>
     <h1>{data.category.data.attributes.name}</h1>
     <Row>
-      {data.categories.data.attributes.portfolios.data.map(item => (
+      {data.category.data.attributes.portfolios.data.map(item => (
         <Col key={item.id} md={4} className='mb-5'>
+          <Card style={{  boxShadow: "0 6px 10px 0 rgba(0,0,0,0.2)"}} className='card-img h-100'>
+            {/* <Card.Img variant='top' src={`${process.env.REACT_APP_ADMIN_BASE_URL}${item.attributes.image.data.attributes.url}`}></Card.Img> */}
+            <Card.Img variant='top' src={`${process.env.REACT_APP_ADMIN_BASE_URL}${item.attributes.image.data.attributes.url}`}></Card.Img>
+            <Card.Body className='p-3'>
+              <Card.Title className='mb-2'>{item.attributes.title}</Card.Title>
+              <Card.Subtitle>{item.attributes.description.substring(0,100)}... <Card.Link as={Link} to={`/portfolio/${item.id}`}>Read More</Card.Link></Card.Subtitle>
+            </Card.Body>
+          </Card>
         </Col>
       ))}
     </Row>
+    </Container>
   </div>
 )
 }
