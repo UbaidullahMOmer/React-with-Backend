f2568c5340a4ed9e3ae1ef6f197a931856c73f26
Heart <121765405+UbaidullahTechnologies@users.noreply.github.com>
2023-02-13 19:28:22 +0500

    remove extra import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react'
-import SinglePortfolio from './pages/SinglePortfolio';
 
-import HomePage from "./pages/HomePage"
-import Tag from "./pages/Tag"
-import Category from "./pages/Category"
-import { ApolloProvider } from '@apollo/client';
-import client from './utils/apoloClient';
-import SiteHeader from './components/SiteHeader';
-import { Container } from 'react-bootstrap';
 import Blog from "./Website/Bolg"
 function App() {
 
