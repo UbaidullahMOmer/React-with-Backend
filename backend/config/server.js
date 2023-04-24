fd3724d3dc1880e2ac3fb27872c8c52001db420b
Heart <researchfigma@gmail.com>
2023-04-24 23:09:32 +0500

    add app keys

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea9a418022c8044305a8ab471025ee7b'),
+    },
   },
 });
